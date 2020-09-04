39514b668c73452a2b295af2f90f4667b80443db
sohamvg <sohamvg11@gmail.com>
2020-09-04 17:49:26 +0530

    Add tests

--- test_input_generator.cpp
+++ test_input_generator.cpp
@@ -6,10 +6,10 @@
 
 using namespace std;
 
-#define ACCESS_REQUESTS 1000
+#define ACCESS_REQUESTS 10000
 #define MAX_MEM_ADDRESS 512
 #define MAX_DATA 1024
-#define DISTINCT_MEM_ADDRESS 20
+#define DISTINCT_MEM_ADDRESS 5
 #define R 1
 #define W 0
 
@@ -41,23 +41,26 @@ int get_random_from_freq(vector<int> &arr, vector<int> &freq)
 int main()
 {
     ofstream inputfile;
-    inputfile.open("input/inp_gen_large0.txt");
+    string filename = "input/inp_gen_large2.txt";
+    inputfile.open(filename);
     srand(time(0));
 
+    // random memory addresses generated with random frequency
     vector<int> memory_address_vector;
-    vector<int> frequency;
+    vector<int> memory_address_freq;
+    int frequency_total = 0;
 
     for (int i = 0; i < DISTINCT_MEM_ADDRESS; i++)
     {
         memory_address_vector.push_back(rand() % (MAX_MEM_ADDRESS + 1));
-        frequency.push_back(rand() % ((MAX_MEM_ADDRESS / 4) + 1));
+        int rand_freq = rand() % (MAX_MEM_ADDRESS + 1);
+        memory_address_freq.push_back(rand_freq);
+        frequency_total += rand_freq;
     }
 
-    int frequency_total = 0;
-    for (auto i = frequency.begin(); i != frequency.end(); i++)
-    {
-        frequency_total += *i;
-    }
+    // read or writes generated with frequency as specified in read_or_write_freq vector
+    vector<int> read_or_write_vector{R, W};
+    vector<int> read_or_write_freq{1, 1};
 
     // generate input file
     inputfile << "16 <cache size in bytes>\n2 <cache block size in bytes>\n2 <cache associativity>\n4 <T>\n";
@@ -65,8 +68,8 @@ int main()
     int total_reads = 0;
     for (int i = 0; i < ACCESS_REQUESTS; i++)
     {
-        int memory_address = get_random_from_freq(memory_address_vector, frequency);
-        int read_or_write = rand() % 2;
+        int memory_address = get_random_from_freq(memory_address_vector, memory_address_freq);
+        int read_or_write = get_random_from_freq(read_or_write_vector, read_or_write_freq);
         if (read_or_write == R)
         {
             total_reads++;
@@ -78,13 +81,13 @@ int main()
         }
     }
 
-    cout << "Input generated with: " << endl;
+    cout << "Input file " << filename << " " << "generated with: " << endl;
     cout << "Number of memory access requests: " << ACCESS_REQUESTS << endl;
     cout << "Read requests: " << total_reads << "   Write requests: " << ACCESS_REQUESTS - total_reads << endl;
     cout << "Memory addresses & their freqeuncy in access requests: " << endl;
     for (int i = 0; i < memory_address_vector.size(); i++)
     {
-        cout << i+1 << ") memory address: " << memory_address_vector.at(i) << "      frequency: " << (float)frequency.at(i) * 100 / frequency_total << " %" << endl;
+        cout << i+1 << ") memory address: " << memory_address_vector.at(i) << "      frequency: " << (float)memory_address_freq.at(i) * 100 / frequency_total << " %" << endl;
     }
 
     inputfile.close();
